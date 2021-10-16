037524bfb9adb7c7cee06969b9a5db7d5bf1ff6f
welove-t <tlove.tky@gmail.com>
2021-10-16 16:36:02 +0900

    ♻️ メイン部分のコンポーネント化

---
+++ components/Main.js
@@ -0,0 +1,15 @@
+import { Headline } from '../components/Headline';
+import { Links } from '../components/Links';
+import styles from '../styles/Home.module.css';
+
+export function Main(props) {
+  return (
+    <main className={styles.main}>
+      <Headline page={props.page}>
+        <code className={styles.code}>pages/{props.page}.js</code>
+      </Headline>
+
+      <Links />
+    </main>
+  );
+}
