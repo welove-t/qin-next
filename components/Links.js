3f4ae71e8d1c0742683f536ca7ec16163a915527
welove-t <tlove.tky@gmail.com>
2021-10-20 07:36:04 +0900

    ♻️ Linksのリファクタリング

--- components/Links.js
+++ components/Links.js
@@ -1,9 +1,41 @@
 import classes from './Links.module.css';
 
+const ITEMS = [
+  {
+    href: 'https://nextjs.org/docs',
+    title: 'Documentation →',
+    description: 'Find in-depth information about Next.js features and API.',
+  },
+  {
+    href: 'https://nextjs.org/learn',
+    title: 'Learn →',
+    description: 'Learn about Next.js in an interactive course with quizzes!',
+  },
+  {
+    href: 'https://github.com/vercel/next.js/tree/master/examples',
+    title: 'Examples →',
+    description: 'Discover and deploy boilerplate example Next.js projects.',
+  },
+  {
+    href: 'https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app',
+    title: 'Deploy →',
+    description:
+      'Instantly deploy your Next.js site to a public URL with Vercel.',
+  },
+];
+
 export function Links() {
   return (
     <div className={classes.grid}>
-      <a href='https://nextjs.org/docs' className={classes.card}>
+      {ITEMS.map((item) => {
+        return (
+          <a key={item.href} href={item.href} className={classes.card}>
+            <h3 className={classes.title}>{item.title}</h3>
+            <p className={classes.description}>{item.description}</p>
+          </a>
+        );
+      })}
+      {/* <a href='https://nextjs.org/docs' className={classes.card}>
         <h3 className={classes.title}>Documentation &rarr;</h3>
         <p className={classes.description}>
           Find in-depth information about Next.js features and API.
@@ -35,7 +67,7 @@ export function Links() {
         <p className={classes.description}>
           Instantly deploy your Next.js site to a public URL with Vercel.
         </p>
-      </a>
+      </a> */}
     </div>
   );
 }
