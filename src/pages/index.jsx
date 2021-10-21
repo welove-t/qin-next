4a8b39092908b1dc7a7337d9a005649fa8c5097f
welove-t <tlove.tky@gmail.com>
2021-10-22 06:50:09 +0900

    ✨ useStateの文字列と真偽値の設定

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -7,11 +7,25 @@ import styles from 'src/styles/Home.module.css';
 
 export default function Home() {
   const [count, setCount] = useState(1);
+  const [text, setText] = useState('');
+  const [isShow, setIsShow] = useState(true);
   const handleClick = useCallback(() => {
     if (count < 10) {
       setCount((count) => count + 1);
     }
   }, [count]);
+
+  const handleDisplay = useCallback(() => {
+    setIsShow((isShow) => !isShow);
+  }, []);
+
+  const handleChange = useCallback((e) => {
+    if (e.target.value.length > 5) {
+      alert('5文字以内にしてください');
+      return;
+    }
+    setText(e.target.value);
+  }, []);
 
   useEffect(() => {
     document.body.style.backgroundColor = 'lightBlue';
@@ -19,14 +33,17 @@ export default function Home() {
       document.body.style.backgroundColor = '';
     };
   }, [count]);
+
   return (
     <div className={styles.container}>
       <Head>
         <title>Index Page</title>
       </Head>
       <Header />
-      <h1>{count}</h1>
+      {isShow ? <h1>{count}</h1> : null}
       <button onClick={handleClick}>ボタン</button>
+      <button onClick={handleDisplay}>{isShow ? '非表示' : '表示'}</button>
+      <input type='text' value={text} onChange={handleChange} />
       <Main page='index' />
 
       <Footer />
