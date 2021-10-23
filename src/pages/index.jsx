11e9d99334310c4f96e0d9decbaf8e78fe006437
welove-t <tlove.tky@gmail.com>
2021-10-23 15:13:18 +0900

    🚸 APIを叩く際に、ローディング状態、エラー状態を追加

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -4,33 +4,17 @@ import { Footer } from 'src/components/Footer';
 import { Header } from 'src/components/Header';
 import { Main } from 'src/components/Main';
 import { useCallback, useEffect, useState } from 'react';
+import { Posts } from 'src/components/Posts';
 
 const Home = (props) => {
-  const [posts, setPosts] = useState([]);
-  const getPosts = useCallback(async () => {
-    const res = await fetch('https://jsonplaceholder.typicode.com/posts');
-    const json = await res.json();
-    setPosts(json);
-  }, []);
-
-  useEffect(() => {
-    getPosts();
-  }, [getPosts]);
-
-  console.log(posts);
   return (
     <div className={styles.container}>
       <Head>
         <title>Index Page</title>
       </Head>
       <Header />
-      {posts.length > 0 ? (
-        <ol>
-          {posts.map((post) => {
-            return <li key={post.id}>{post.title}</li>;
-          })}
-        </ol>
-      ) : null}
+
+      <Posts />
     </div>
   );
 };
