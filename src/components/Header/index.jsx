2f132794ad3b0342e35368e8b0eeb936ee96551e
welove-t <tlove.tky@gmail.com>
2021-10-25 06:01:28 +0900

    🚚 投稿系の名前をpostsに変更

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -2,7 +2,7 @@ import classes from 'src/components/Header/Header.module.css';
 import Link from 'next/link';
 
 const NAV_ITEMS = [
-  { href: '/', label: 'Index' },
+  { href: '/posts', label: 'Posts' },
   { href: '/about', label: 'About' },
 ];
 
