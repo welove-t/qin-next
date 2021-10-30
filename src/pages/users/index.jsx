f258e2139ee6ba7d84f3bf183f14c2e7b2429d67
welove-t <tlove.tky@gmail.com>
2021-10-30 15:52:24 +0900

    ✨ ユーザー一覧のSSR対応

--- src/pages/users/index.jsx
+++ src/pages/users/index.jsx
@@ -1,16 +1,33 @@
 import Head from 'next/head';
 import { Header } from 'src/components/Header';
 import { Users as UsersComponent } from 'src/components/Users';
+import { SWRConfig } from 'swr';
 
-const Users = () => {
+export const getServerSideProps = async () => {
+  const USERS_API_URL = `https://jsonplaceholder.typicode.com/users`;
+  const users = await fetch(USERS_API_URL);
+  const usersData = await users.json();
+
+  return {
+    props: {
+      fallback: {
+        [USERS_API_URL]: usersData,
+      },
+    },
+  };
+};
+
+const Users = (props) => {
+  const { fallback } = props;
   return (
     <div>
       <Head>
         <title>Users Page</title>
       </Head>
-      <Header />
-
-      <UsersComponent />
+      <SWRConfig value={{ fallback }}>
+        <Header />
+        <UsersComponent />
+      </SWRConfig>
     </div>
   );
 };
