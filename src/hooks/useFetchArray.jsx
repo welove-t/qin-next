f741c8c48420354df877f0ccfa5df6c4790e79c4
welove-t <tlove.tky@gmail.com>
2021-10-30 11:50:32 +0900

    ♻️ 並び替え

--- src/hooks/useFetchArray.jsx
+++ src/hooks/useFetchArray.jsx
@@ -12,22 +12,26 @@ export const useFetchArray = (url) => {
 };
 
 const API_URL = 'https://jsonplaceholder.typicode.com';
-export const useComments = () => {
-  return useFetchArray(`${API_URL}/comments`);
-};
 
+// posts
 export const usePosts = () => {
   return useFetchArray(`${API_URL}/posts`);
 };
 
-export const useUsers = () => {
-  return useFetchArray(`${API_URL}/users`);
+export const usePostsByUserId = (id) => {
+  return useFetchArray(id ? `${API_URL}/posts?userId=${id}` : null);
+};
+
+// comments
+export const useComments = () => {
+  return useFetchArray(`${API_URL}/comments`);
 };
 
 export const useCommentsByPostId = (id) => {
   return useFetchArray(id ? `${API_URL}/comments?postId=${id}` : null);
 };
 
-export const usePostsByUserId = (id) => {
-  return useFetchArray(id ? `${API_URL}/posts?userId=${id}` : null);
+// users
+export const useUsers = () => {
+  return useFetchArray(`${API_URL}/users`);
 };
