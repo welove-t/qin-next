5868f031a286c8c340dae71842a0abbe22e2eb06
welove-t <tlove.tky@gmail.com>
2021-10-30 11:07:39 +0900

    🔥 useUserのPosts取得を削除

--- src/hooks/useUser.jsx
+++ src/hooks/useUser.jsx
@@ -4,24 +4,16 @@ import useSWR from 'swr';
 
 export const useUser = () => {
   const router = useRouter();
-  const { data: user, error: userError } = useSWR(
+  const { data, error } = useSWR(
     router.query.id
       ? `https://jsonplaceholder.typicode.com/users/${router.query.id}`
       : null,
     fetcher
   );
 
-  const { data: posts, error: postsError } = useSWR(
-    user?.id
-      ? `https://jsonplaceholder.typicode.com/posts?userId=${user.id}`
-      : null,
-    fetcher
-  );
-
   return {
-    posts,
-    user,
-    error: postsError || userError,
-    isLoading: !user && !userError,
+    data,
+    error,
+    isLoading: !data && !error,
   };
 };
