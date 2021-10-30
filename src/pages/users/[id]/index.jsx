d1cca823b813760efd56f2965492472262922c88
welove-t <tlove.tky@gmail.com>
2021-10-30 14:56:40 +0900

    ✨ ユーザー詳細ページでSSRを実践

--- src/pages/users/[id]/index.jsx
+++ src/pages/users/[id]/index.jsx
@@ -1,13 +1,28 @@
 import { Header } from 'src/components/Header';
 import { User } from 'src/components/User';
+import { SWRConfig } from 'swr';
 
-const UsersId = () => {
+export const getServerSideProps = async (ctx) => {
+  const { id } = ctx.query;
+  const API_URL = `https://jsonplaceholder.typicode.com/users/${id}`;
+  const user = await fetch(API_URL);
+  const userData = await user.json();
+  return {
+    props: {
+      fallback: {
+        [API_URL]: userData,
+      },
+    },
+  };
+};
+
+const UsersId = (props) => {
+  const { fallback } = props;
   return (
-    <div>
+    <SWRConfig value={{ fallback }}>
       <Header />
-
       <User />
-    </div>
+    </SWRConfig>
   );
 };
 export default UsersId;
