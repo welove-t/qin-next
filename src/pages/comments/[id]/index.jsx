784856a8f9317949503955c9a58b0ed63ca60be9
welove-t <tlove.tky@gmail.com>
2021-11-03 11:41:00 +0900

    ⚡️ comment個別ページのSG化

--- src/pages/comments/[id]/index.jsx
+++ src/pages/comments/[id]/index.jsx
@@ -1,12 +1,47 @@
 import { Header } from 'src/components/Header';
 import { Comment } from 'src/components/Comment';
+import { SWRConfig } from 'swr';
 
-const CommentsId = () => {
+export const getStaticPaths = async () => {
+  const comments = await fetch('https://jsonplaceholder.typicode.com/comments');
+  const commentsData = await comments.json();
+  const paths = commentsData.map((comment) => ({
+    params: {
+      id: comment.id.toString(),
+    },
+  }));
+
+  return {
+    paths,
+    fallback: false,
+  };
+};
+
+export const getStaticProps = async (ctx) => {
+  const { id } = ctx.params;
+  const COMMENT_API_URL = `https://jsonplaceholder.typicode.com/comments/${id}`;
+  console.log('SGのページ');
+  const comment = await fetch(COMMENT_API_URL);
+  const commentData = await comment.json();
+
+  return {
+    props: {
+      fallback: {
+        [COMMENT_API_URL]: commentData,
+      },
+    },
+  };
+};
+
+const CommentsId = (props) => {
+  const { fallback } = props;
+
   return (
     <div>
-      <Header />
-
-      <Comment />
+      <SWRConfig value={{ fallback }}>
+        <Header />
+        <Comment />
+      </SWRConfig>
     </div>
   );
 };
